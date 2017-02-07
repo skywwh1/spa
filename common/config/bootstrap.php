76330f1b50b11a6bfff282eb5fbff688ec5e8669
iven.wu <iven.wu@nordanglia.com>
2017-02-07 17:50:29 +0800

    api module

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,3 +4,4 @@ Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@track','https://track.superads.cn');
+Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
