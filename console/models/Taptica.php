a74aaa887d8209c2daed5734101e2d77dcd1892d
skywwh1 <skywwh1@126.com>
2017-07-07 10:55:38 +0800

    tapica

--- console/models/Taptica.php
+++ console/models/Taptica.php
@@ -71,6 +71,13 @@ class Taptica
             $camp->adv_link = str_replace('&s1={Sub_ID}&s2={Clickid}&s3={Sub3}&s4={Sub4}&s5={Sub5}&udid={GAID/IDFA}', '', $adv_link);
             $camp->package_name = $model->package_name;
             $camp->platform = $model->platform;
+            if(strpos($camp->platform,'Android')){
+                $camp->platform = 'android';
+            }
+            if(strpos($camp->platform,'IPhone')){
+                $camp->platform = 'ios';
+            }
+            $camp->traffic_source='non-incent,no adult';
             $camp->description = $model->description;
             $camp->description = strip_tags($camp->description);
             $camp->description = str_replace('&nbsp;', '', $camp->description);
@@ -146,7 +153,7 @@ class Taptica
                         }
                         $item->SupportedCountriesV2 = rtrim($aa,',');
                     }
-                    if ($item->SupportedCountriesV2 == 'ww,') {
+                    if ($item->SupportedCountriesV2 == 'ww') {
                         $item->SupportedCountriesV2 = 'Global';
                     }
                     $newOffers[] = $item;
