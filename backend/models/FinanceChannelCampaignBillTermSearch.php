62ba0d3e14b45dc1369b6a341526c01b006f7573
cttlhy <2539131080@qq.com>
2017-09-19 14:07:53 +0800

    api

--- backend/models/FinanceChannelCampaignBillTermSearch.php
+++ backend/models/FinanceChannelCampaignBillTermSearch.php
@@ -160,7 +160,6 @@ class FinanceChannelCampaignBillTermSearch extends FinanceChannelCampaignBillTer
 
         $query->andFilterWhere(['like', 'fcb.time_zone', $this->time_zone])
             ->andFilterWhere(['like', 'camp.campaign_name', $this->campaign_name])
-            ->andFilterWhere(['<>', 'fd.status', 0])
             ->andFilterWhere(['<>', 'fcb.cost', 0]);
 
 //        var_dump($query->createCommand()->sql);
